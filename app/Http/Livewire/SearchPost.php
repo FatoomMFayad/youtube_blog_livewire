1c7ceaa3047819bf021e038b1f06e24de7784a97
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-23 19:32:52 +0200

    get query string in SearchPost Controller

--- app/Http/Livewire/SearchPost.php
+++ app/Http/Livewire/SearchPost.php
@@ -8,8 +8,13 @@ use Livewire\Component;
 class SearchPost extends Component
 {
     public $query = '';
+    public $results;
+    protected $queryString = ['query'];
     public function render()
     {
+        if(strlen($this->query) > 2 ){
+            $this->results = Post::where('title', 'like', `%{$this->query}%`)->get();
+        }
         return view('livewire.search-post');
     }
 }
