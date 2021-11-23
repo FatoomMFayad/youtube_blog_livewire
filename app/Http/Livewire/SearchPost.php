ebe932f29b1c200a40cded6c2c46048cb485db6c
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-23 19:26:45 +0200

    search component functionality

--- app/Http/Livewire/SearchPost.php
+++ app/Http/Livewire/SearchPost.php
@@ -8,19 +8,8 @@ use Livewire\Component;
 class SearchPost extends Component
 {
     public $query = '';
-    public $results;
-    protected $queryString = [ 'query' => ['except' => '']];
-
-    public function mount()
-    {
-        $this->reset();
-    }
-
     public function render()
     {
-        if (strlen($this->query) > 2){
-            $this->results = Post::where('title', 'like', "%{$this->query}%")->get();
-        }
         return view('livewire.search-post');
     }
 }
